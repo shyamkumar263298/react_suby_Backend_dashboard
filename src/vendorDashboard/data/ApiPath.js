090bd31f4b01e9e75f2cb56572eeafccf7bffeaa
nalamshyamkumar <shyamkumar06042000@gmail.com>
2025-04-22 09:48:19 +0530

    / added at api url

--- src/vendorDashboard/data/ApiPath.js
+++ src/vendorDashboard/data/ApiPath.js
@@ -1,4 +1,4 @@
 // export const API_PATH = "http://localhost:4000/"
 
-export const API_PATH = "https://backend-nodejs-suby-swpo.onrender.com"
+export const API_PATH = "https://backend-nodejs-suby-swpo.onrender.com/"
 
